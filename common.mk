717a50cad1d004b48a2def3aa450d6a4b80c698d
Ricky Salinas <rsalinas1@mail.usf.edu>
2014-04-12 03:04:48 -0400

    t0lte: clean up device tree
    Clean up overlays and cm.mk
    
    Change-Id: I371505cbe4ba9ccbf2aac765f2ea1b4f13155df5
    Signed-off-by: Ricky Salinas <rsalinas1@mail.usf.edu>

--- common.mk
+++ common.mk
@@ -74,8 +74,6 @@ PRODUCT_PACKAGES += \
     Stk \
     SamsungServiceMode
 
-$(call inherit-product-if-exists, vendor/cm/config/nfc_enhanced.mk)
-
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=SamsungQualcommRIL \
